6f57ac15bd8ede371bc552dfbe187a3069e2eae7
smendez92 <samuelremndez14@gmail.com>
2017-05-22 00:21:43 -0500

    basic game functions in place alongside design to help me think

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@ var fighterOne = {
 	fighterName: "Princess Poison",
 	healthPoints: 100,
 	attackPower: 10,
-	counterattackPower: 11,
+	counterattackPower: 10,
 	fighterPicture: "http://www.lorempixel.com/150/150/abstract"
 };
 
@@ -12,7 +12,7 @@ var fighterTwo = {
 	fighterName: "Duke Dynamite",
 	healthPoints: 120,
 	attackPower: 20,
-	counterattackPower: 12,
+	counterattackPower: 20,
 	fighterPicture: "http://www.lorempixel.com/150/150/people"
 };
 
@@ -21,7 +21,7 @@ var fighterThree = {
 	fighterName: "Lord LaserEyes",
 	healthPoints: 140,
 	attackPower: 30,
-	counterattackPower: 13,
+	counterattackPower: 30,
 	fighterPicture: "http://www.lorempixel.com/150/150/food"
 };
 
@@ -30,66 +30,29 @@ var fighterFour = {
 	fighterName: "Empress ElectricShock",
 	healthPoints: 180,
 	attackPower: 40,
-	counterattackPower: 14,
+	counterattackPower: 40,
 	fighterPicture: "http://www.lorempixel.com/150/150/city"
 };
 
 //set array of fighter options
 var fightersArray = [fighterOne, fighterTwo, fighterThree, fighterFour];
 
-//JQuery-dependent function that iterates through fightersArray and appends it to the "fighters-container" already baked into our Bootstrap-powered HTML
-function loadFightersSection(){
-		for (i = 0; i < fightersArray.length; i++){
-		
-		//Obtain fighter stats from each fighter in our game.js file
-		var currentFighter = fightersArray[i];
-		var displayFighterName = currentFighter.fighterName;
-		var displayFighterHP = currentFighter.healthPoints;
-		var displayFighterAttack = currentFighter.attackPower;
-		var fighterDetailsImageLink = currentFighter.fighterPicture;
 
-		//dynamically create an HTML container to display each fighter's stats
-		var fighterProfile = $("<div>");
-		fighterProfile.addClass("col-xs-12 col-sm-6 col-md-6 col-lg-6");
-		fighterProfile.attr("idNumberValue", i);
-
-		//create a fighter image container
-		var fighterImageDiv = $("<figure>");
-
-		//create the fighter stats "Details" dropdown div.
-		var fighterDetails = $("<div>");
-		var fighterDetailsName = $("<div>");
-		var fighterDetailsHP = $("<div>");
-		var fighterDetailsAttack = $("<div>");
-
-		fighterImageDiv.html("<img class='img-fluid JS-fighter-picture' src='" + fighterDetailsImageLink + "' value='" + currentFighter.idNumber + "'>");
-		fighterProfile.append(fighterImageDiv);
-
-		fighterDetailsName.html("Name: " + displayFighterName);
-		fighterDetailsHP.html("HP: " + displayFighterHP);
-		fighterDetailsAttack.html("Attack: " + displayFighterAttack);
-
-		fighterDetails.append(fighterDetailsName);
-		fighterDetails.append(fighterDetailsHP);
-		fighterDetails.append(fighterDetailsAttack);
-		fighterProfile.append(fighterDetails);
-		$("#fighters-container").append(fighterProfile);
-		}
-};
-
-//function takes in a fighter's ID number and a target div on the webpage, loading the corresponding fighter into the target div
-function loadUserSelection(userSelectionIdNumber, webpageTargetDiv){
+/*JQuery-dependent function that takes in a fighter's ID number and a target div on the webpage, loading the corresponding fighter into the target div.
+It also assigns the fighter's image a team id: "user" "opponent" or "null"
+*/
+function loadFighterProfile(fighterIdNumber, webpageTargetDiv, teamName){
 		//Obtain fighter stats for user selection
-		var currentFighter = fightersArray[userSelectionIdNumber];
+		var currentFighter = fightersArray[fighterIdNumber];
 		var displayFighterName = currentFighter.fighterName;
 		var displayFighterHP = currentFighter.healthPoints;
 		var displayFighterAttack = currentFighter.attackPower;
 		var fighterDetailsImageLink = currentFighter.fighterPicture;
 
-		//dynamically HTML container to display the fighter's stats
+		//dynamically create HTML container to display the fighter's stats
 		var fighterProfile = $("<div>");
-		fighterProfile.addClass("col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-10 col-md-offset-1 col-lg-10 col-lg-offset-1");
-		fighterProfile.attr("idNumberValue", i);
+		fighterProfile.addClass("col-xs-12 col-sm-6 col-sm-offset-3 col-md-6 col-md-offset3 col-lg-6 col-lg-offset-3 fighter-profile")
+		fighterProfile.attr("fighterIdValueNum", fighterIdNumber);
 
 		//create a fighter image container
 		var fighterImageDiv = $("<figure>");
@@ -105,7 +68,11 @@ function loadUserSelection(userSelectionIdNumber, webpageTargetDiv){
 
 		fighterDetailsName.html("Name: " + displayFighterName);
 		fighterDetailsHP.html("HP: " + displayFighterHP);
+		fighterDetailsHP.addClass("JS-stats-HP");
+		fighterDetailsHP.attr("id", teamName);
 		fighterDetailsAttack.html("Attack: " + displayFighterAttack);
+		fighterDetailsAttack.addClass("JS-stats-attack");
+		fighterDetailsAttack.attr("id", teamName);
 
 		fighterDetails.append(fighterDetailsName);
 		fighterDetails.append(fighterDetailsHP);
